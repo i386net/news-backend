3ff53928052482b3f86fa55704b28ec7ae308bf2
DK <i386.net@gmail.com>
2020-08-05 12:11:41 +0300

    add: user model

---
+++ models/user.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const mongooseValidator = require('mongoose-unique-validator');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const { emailValidationOptions } = require('../configs/appdata');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: [2, 'Минимальная длина имени 2 символа'],
+    maxlength: [30, 'Максимальная длина имени 30 символов'],
+    required: true,
+
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (email) => validator.isEmail(email, emailValidationOptions),
+      message: (props) => `${props.value} некорректная почта`,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неверная почта или пароль'));
+      }
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject((new Error('Неверная почта или пароль')));
+        }
+        return user;
+      });
+    });
+};
+userSchema.plugin(mongooseValidator, { message: 'Пользователь с таким адресом уже существует.' });
+
+module.exports = mongoose.model('user', userSchema);
