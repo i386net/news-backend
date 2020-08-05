cacf554bd99acdf73ecd173adc2b00ecae0d5096
DK <i386.net@gmail.com>
2020-08-05 19:54:31 +0300

    add: key and messages files

---
+++ configs/jwtdata.js
@@ -0,0 +1,2 @@
+const { JWT_SECRET, NODE_ENV } = process.env;
+module.exports = { key: NODE_ENV === 'production' ? JWT_SECRET : 'secret-key' };
--- configs/messages.js
+++ configs/messages.js
@@ -1,1 +1,29 @@
-module.exports = { dbConnectionError: 'Невозможно установить соединение с базой данных' };
+const joiMessage = {
+  nameMin: 'Название должно быть не менее {#limit} знаков',
+  nameMax: 'Название должно быть не более {#limit} знаков',
+  nameEmpty: 'Название не может быть пустым',
+  incorrectURL: 'Некорректная ссылка',
+  urlRequired: 'Ссылка обязательное поле',
+  emptyURL: 'Ссылка не может быть пустой',
+  incorrectFormatID: 'Некорректный формат id',
+  userNameString: 'Имя должно быть строкой',
+  usernameRequired: 'Имя обязательное поле',
+  userNameMin: 'В имени должно быть не менее {#limit} знаков',
+  userNameMax: 'В имени должно быть не более {#limit} знаков',
+  userNameEmpty: 'Имя не может быть пустым',
+  emptyParam: 'Поле не может быть пустым',
+  requiredParam: 'Обязательное поле',
+  emailNotValid: 'Данный адрес не является электронной почтой',
+
+};
+const statusMessage = {
+  dbConnectionError: 'Невозможно установить соединение с базой данных',
+  resourseNotFoundError: 'Ресурс не найден',
+  userLoginBusy: 'Этот логин занят',
+  userIncorrectData: 'Переданы некорректные данные',
+  userWelcomeMessage: 'Добро пожаловать <(￣︶￣)>',
+  userAuthError: 'Ошибка авторизации (×﹏×)',
+
+};
+
+module.exports = { joiMessage, statusMessage };
