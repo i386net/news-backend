4c0ad654441bca519d858f3e166f4caa275f8b59
DK <i386.net@gmail.com>
2020-08-06 13:04:11 +0300

    update: add celebrate options

--- configs/appdata.js
+++ configs/appdata.js
@@ -1,9 +1,14 @@
+const { Joi } = require('celebrate');
+const { joiMessage } = require('./messages');
+
 const {
   NODE_ENV, PORT, WEB_HOST, DB_HOST,
 } = process.env;
+
 let webAddress;
 let port;
 let dbAddress;
+
 const dbOptions = {
   useNewUrlParser: true,
   useCreateIndex: true,
@@ -22,6 +27,59 @@ const urlValidationOptions = {
 const emailValidationOptions = {
   require_tld: true,
 };
+const signupValidationOptions = {
+  body: Joi.object().keys({
+    name: Joi.string()
+      .required()
+      .min(2)
+      .max(30)
+      .pattern(/^[a-zA-Zа-яА-ЯёЁ -]*$/)
+      .messages({
+        'string.pattern.base': 'Имя должно быть строкой',
+        'any.required': joiMessage.usernameRequired,
+        'string.min': joiMessage.userNameMin,
+        'string.max': joiMessage.userNameMax,
+        'string.empty': joiMessage.userNameEmpty,
+      }),
+    email: Joi.string()
+      .required()
+      .email()
+      .messages({
+        'any.required': joiMessage.requiredParam,
+        'string.email': joiMessage.emailNotValid,
+        'string.empty': joiMessage.emptyParam,
+      }),
+    password: Joi.string().pattern(/^[a-zA-Z0-9]*$/)
+      .min(8)
+      .max(16)
+      .required()
+      .messages(
+        {
+          'string.pattern.base': joiMessage.passwordAlphaNum,
+          'string.min': joiMessage.passwordMin,
+          'string.max': joiMessage.passwordMax,
+          'string.empty': joiMessage.emptyParam,
+        },
+      )
+    ,
+  }),
+};
+const signinValidationOptions = {
+  body: Joi.object().keys({
+    email: Joi.string().email().required()
+      .messages({
+        'string.empty': joiMessage.emptyParam,
+        'any.required': joiMessage.requiredParam,
+        'string.email': joiMessage.emailNotValid,
+      }),
+    password: Joi.string().required()
+      .messages({
+        'any.required': joiMessage.requiredParam,
+        'string.empty': joiMessage.emptyParam,
+      }),
+
+  }),
+};
 
 if (NODE_ENV !== 'production') {
   webAddress = 'http://localhost' || WEB_HOST;
@@ -30,5 +88,12 @@ if (NODE_ENV !== 'production') {
 }
 
 module.exports = {
-  webAddress, port, dbAddress, dbOptions, urlValidationOptions, emailValidationOptions,
+  webAddress,
+  port,
+  dbAddress,
+  dbOptions,
+  urlValidationOptions,
+  emailValidationOptions,
+  signupValidationOptions,
+  signinValidationOptions,
 };
