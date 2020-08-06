d4cf5f9661f95a10d117a2393af9dc36a30fc4c1
DK <i386.net@gmail.com>
2020-08-06 09:08:33 +0300

    fix: type error

--- middlewares/wrongUrlHandler.js
+++ middlewares/wrongUrlHandler.js
@@ -2,5 +2,5 @@ const { statusMessage } = require('../configs/messages');
 const { NotFoundError } = require('../errors/errors');
 
 module.exports.wrongUrlHandler = (req, res, next) => {
-  next(new NotFoundError(statusMessage.resourseNotFoundError));
+  next(new NotFoundError(statusMessage.resourceNotFoundError));
 };
