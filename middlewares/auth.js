3175c0ead3810a53f77e7da5c8fa4390f2eab501
DK <i386.net@gmail.com>
2020-08-06 09:17:28 +0300

    add: some todos

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,14 +4,14 @@ const { UnauthorizedError } = require('../errors/errors');
 
 module.exports = (req, res, next) => {
   if (!req.cookies.jwt) {
-    return next(new UnauthorizedError('Необходима авторизация'));
+    return next(new UnauthorizedError('Необходима авторизация')); // todo поменять на ошибку из словаря
   }
   const token = req.cookies.jwt;
   let payload;
   try {
     payload = jwt.verify(token, key);
   } catch (err) {
-    return next(new UnauthorizedError('Необходима авторизация'));
+    return next(new UnauthorizedError('Необходима авторизация')); // todo поменять на ошибку из словаря
   }
   req.user = payload;
   return next();
