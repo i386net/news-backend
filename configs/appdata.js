9efe00630bc76e4071b071a220af69d76f2c6720
DK <i386.net@gmail.com>
2020-08-07 19:43:25 +0300

    delete: remove environment variables from appdata

--- configs/appdata.js
+++ configs/appdata.js
@@ -1,14 +1,6 @@
 const { Joi } = require('celebrate');
 const { joiMessage } = require('./messages');
 
-const {
-  NODE_ENV, PORT, WEB_HOST, DB_HOST,
-} = process.env;
-
-let webAddress;
-let port;
-let dbAddress;
-
 const dbOptions = {
   useNewUrlParser: true,
   useCreateIndex: true,
@@ -81,16 +73,7 @@ const signinValidationOptions = {
   }),
 };
 
-if (NODE_ENV !== 'production') {
-  webAddress = 'http://localhost' || WEB_HOST;
-  port = 3000 || PORT;
-  dbAddress = 'mongodb://localhost:27017/newsdb' || DB_HOST;
-}
-
 module.exports = {
-  webAddress,
-  port,
-  dbAddress,
   dbOptions,
   urlValidationOptions,
   emailValidationOptions,
