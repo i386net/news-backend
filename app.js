2041a190b0256cc1ea17f4c3134b8c5c2914e830
DK <i386.net@gmail.com>
2020-08-09 12:52:42 +0300

    remove: dotenv import
    update: change env vars to uppercase
    add: timestamp for console.log

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
@@ -17,10 +16,12 @@ const { userRouter, articleRouter } = require('./routes/index.js');
 const auth = require('./middlewares/auth');
 const { apiLimiter, createAccountLimiter, loginLimiter } = require('./middlewares/rateLimiter');
 
+const timestamp = (new Date(Date.now())).toLocaleTimeString();
+
 const {
-  port = 3000,
-  webHost = 'http://localhost',
-  dbHost = 'mongodb://localhost:27017/newsdb',
+  PORT = 3000,
+  WEB_HOST = 'http://localhost',
+  DB_HOST = 'mongodb://localhost:27017/newsdb',
 } = process.env;
 
 const app = express();
@@ -29,8 +30,8 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookeParser());
-mongoose.connect(dbHost, dbOptions)
-  .then(() => console.log(`DB connection on: ${dbHost}`))
+mongoose.connect(DB_HOST, dbOptions)
+  .then(() => console.log(`${timestamp} DB connection on: ${DB_HOST}`))
   .catch(() => new Error(statusMessage.dbConnectionError));
 app.use(requestsLogger);
 
@@ -46,4 +47,4 @@ app.use(errors());
 app.use('*', wrongUrlHandler);
 app.use(errorsHandler);
 
-app.listen(port, () => console.log(`Сервер запущен по адресу: ${webHost}:${port}`));
+app.listen(PORT, () => console.log(`${timestamp} Сервер запущен по адресу: ${WEB_HOST}:${PORT}`));
