08a5ea057aad013cb3b8033c66b1fdb2fbe66788
DK <i386.net@gmail.com>
2020-08-09 20:42:15 +0300

    update: new article returns selected data
    add: article id verification

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,11 +1,23 @@
+const mongoose = require('mongoose');
 const Article = require('../models/article');
-const { NotFoundError, ForbiddenError } = require('../errors/errors');
+const { NotFoundError, ForbiddenError, BadRequestError } = require('../errors/errors');
 const { statusMessage } = require('../configs/messages');
 
 const createArticle = (req, res, next) => {
   const newArticle = { ...req.body };
-  Article.create({ ...newArticle, owner: req.user._id })
-    .then((article) => res.send({ data: article }))
+  Article.create({
+    ...newArticle,
+    owner: req.user._id,
+  })
+    .then((article) => res.send({
+      keyword: article.keyword,
+      title: article.title,
+      text: article.text,
+      link: article.link,
+      date: article.date,
+      source: article.source,
+      image: article.image,
+    }))
     .catch(next);
 };
 
@@ -16,21 +28,29 @@ const getArticles = (req, res, next) => {
 };
 
 const deleteArticle = (req, res, next) => {
-  Article.findById(req.params.articleId)
-    .orFail(new NotFoundError(statusMessage.articleNotFoundError))
-    .then((article) => {
-      if (article.owner.toString() === req.user._id) {
-        return Article.findByIdAndDelete(article._id)
-          .orFail(new NotFoundError('not found'))
-          .then((deletedArticle) => res.send({
-            data: deletedArticle,
-            message: statusMessage.articleDeleted,
-          }))
-          .catch(next);
-      }
-      return next(new ForbiddenError(statusMessage.articleForbiddenError));
-    })
-    .catch(next);
+  if (mongoose.Types.ObjectId.isValid(req.params.articleId)) {
+    return Article.findById(req.params.articleId)
+      .select('owner')
+      .orFail(new NotFoundError(statusMessage.articleNotFoundError))
+      .then((article) => {
+        if (article.owner.toString() === req.user._id) {
+          return Article.deleteOne(article)
+            .orFail(new NotFoundError(statusMessage.articleNotFoundError))
+            .then((deletedArticle) => res.send({
+              data: deletedArticle,
+              message: statusMessage.articleDeleted,
+            }))
+            .catch(next);
+        }
+        return next(new ForbiddenError(statusMessage.articleForbiddenError));
+      })
+      .catch(next);
+  }
+  return next(new BadRequestError(statusMessage.articleBadRequestError));
 };
 
-module.exports = { createArticle, getArticles, deleteArticle };
+module.exports = {
+  createArticle,
+  getArticles,
+  deleteArticle,
+};
